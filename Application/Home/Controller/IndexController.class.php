34624341a4ebba4d475a79753c2f2e845d2219b9
wodeccu <wodecc_u@sina.com>
2017-02-01 23:20:12 +0800

    +接受到文字消息后，调用图灵机器人自动回复

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -28,24 +28,69 @@ class IndexController extends Controller {
 	}
 	public function reponseMsg(){
 		$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
-		$postObj = simplexml_load_string($postArr);
+		$postObj = simplexml_load_string($postArr,'SimpleXMLElement',LIBXML_NOCDATA);
+		switch ($postObj->MsgType) {
+			case 'event':
+					if(strtolower($postObj->Event) == 'subscribe'){
+						$toUser = $postObj->FromUserName;
+						$fromUser = $postObj->ToUserName;
+						$time = time();
+						$msgType = 'text';
+						$content = '欢迎关注诗歌班里的小男孩';
+						$template = "<xml>
+									<ToUserName><![CDATA[%s]]></ToUserName>
+									<FromUserName><![CDATA[%s]]></FromUserName>
+									<CreateTime>%s</CreateTime>
+									<MsgType><![CDATA[%s]]></MsgType>
+									<Content><![CDATA[%s]]></Content>
+									</xml>";
+						$info = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
+						echo $info;
+					}
+				break;
+			case 'text':
+					$url  = "http://www.tuling123.com/openapi/api";
+					$data = json_encode(array('key'=>'ce469e196b9d46f0bda1391afe181aab', 'info'=>$postObj->Content,'userid'=>$postObj->FromUserName));
+					$result = $this->http_post_data($url, $data);
+					$result = json_decode($result[1]);
+					$template = "<xml>
+								<ToUserName><![CDATA[%s]]></ToUserName>
+								<FromUserName><![CDATA[%s]]></FromUserName>
+								<CreateTime>%s</CreateTime>
+								<MsgType><![CDATA[%s]]></MsgType>
+								<Content><![CDATA[%s]]></Content>
+								</xml>";
+					$toUser = $postObj->FromUserName;
+					$fromUser = $postObj->ToUserName;
+					$time =time();
+					$msgType = 'text';
+					$content = $postObj->Content;
+					echo $info = sprintf($template,$toUser,$fromUser,$time,$msgType,$result->text);
+				break;
+			default:
+				# code...
+				break;
+		}
 		if($postObj->MsgType == 'event'){
-			if(strtolower($postObj->Event) == 'subscribe'){
-				$toUser = $postObj->FromUserName;
-				$fromUser = $postObj->ToUserName;
-				$time = time();
-				$msgtype = 'text';
-				$content = '1';
-				$template = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							</xml>";
-				$info = sprintf($template,$toUser,$fromUser,$time,$msgtype,$content);
-				echo $info;
-			}
+			
 		}
 	}
+	public function http_post_data($url, $data_string) {
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json; charset=utf-8',
+			'Content-Length: ' . strlen($data_string))
+			);
+	ob_start();
+	curl_exec($ch);
+	echo $return_content = ob_get_contents();
+	ob_end_clean();
+
+	$return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	return array($return_code, $return_content);
+}
 }
